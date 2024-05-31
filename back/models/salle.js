0a35998b5a8a1f18575a0b1ce4d1a9cdbed7747b
jihedbenamara <132092654+jihedbenamara@users.noreply.github.com>
2024-05-31 17:29:30 +0100

    Push

--- back/models/salle.js
+++ back/models/salle.js
@@ -4,7 +4,9 @@ const AutoIncrement = require('mongoose-sequence')(mongoose);
 const salleSchema = new mongoose.Schema({
   salle_id: { type: Number, auto: true },
   num_salle: { type: Number, auto: true, unique:true},
+  name:String,
   nbre_place: Number
+  
 });
 
 salleSchema.plugin(AutoIncrement, { inc_field: 'num_salle' });
