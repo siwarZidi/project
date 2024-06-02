8196d9bce8e7d55f03a52aa5fd904350049479d8
AhmedTrigui137 <123263433+AhmedTrigui137@users.noreply.github.com>
2024-06-02 14:33:25 +0200

    bbb

--- PPP/src/pages/Club/ACM/index.js
+++ PPP/src/pages/Club/ACM/index.js
@@ -34,49 +34,7 @@ function ACM_page() {
       />
 
         <InformationClub />
-        <MKBox pt={18} pb={6}>
-          <Container>
-            <Grid container spacing={3}>
-              <Grid item xs={12} lg={5} ml="auto" sx={{ textAlign: { xs: "center", lg: "left" } }}>
-                <MKTypography variant="h4" color="error" fontWeight="bold" mb={0.5}>
-                  Thank you for your support!
-                </MKTypography>
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                lg={5}
-                my={{ xs: 5, lg: "auto" }}
-                mr={{ xs: 0, lg: "auto" }}
-                sx={{ textAlign: { xs: "center", lg: "right" } }}
-              >
-                <MKSocialButton
-                  component="a"
-                  href="https://twitter.com/intent/tweet?text=Check%20Material%20Design%20System%20made%20by%20%40CreativeTim%20%23webdesign%20%23designsystem%20%23mui5&amp;url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fmaterial-kit-react"
-                  target="_blank"
-                  color="twitter"
-                  sx={{ mr: 1 }}
-                >
-                  <i className="fab fa-twitter" />
-                  &nbsp;Tweet
-                </MKSocialButton>
-                <MKSocialButton
-                  component="a"
-                  href="https://www.facebook.com/sharer/sharer.php?u=https://www.facebook.com/insat.rnu.tn"
-                  target="_blank"
-                  color="facebook"
-                  sx={{ mr: 1 }}
-                >
-                  <i className="fab fa-facebook" />
-                  &nbsp;Share
-                </MKSocialButton>
-              </Grid>
-            </Grid>
-          </Container>
-        </MKBox>
-      <MKBox pt={9} px={1} mt={6}>
-        <DefaultFooter content={footerRoutes} />
-      </MKBox>
+
     </>
   );
 }
